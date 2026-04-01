0056c54fa9889a0f4de6fdce82dde2f5e7d7f840
Degeneracy-Evil <h_p_chen26@163.com>
2026-04-01 21:00:29 +0800

    1. 去除ai写的啰嗦的基础门module 2. 格式化所有代码

--- dev/1alu/rtl/mux.v
+++ dev/1alu/rtl/mux.v
@@ -5,72 +5,73 @@
 // 实现: 使用门级原语，避免被综合为IP核
 module mux_2to1 #(
     parameter WIDTH = 32
-)(
+  )(
     input  [WIDTH-1:0] a,
     input  [WIDTH-1:0] b,
     input              sel,
     output [WIDTH-1:0] y
-);
-    wire [WIDTH-1:0] a_masked;
-    wire [WIDTH-1:0] b_masked;
-    wire [WIDTH-1:0] not_sel_vec;
-    
-    // 对每一位独立实现: y[i] = (a[i] & ~sel) | (b[i] & sel)
-    genvar i;
-    generate
-        for (i = 0; i < WIDTH; i = i + 1) begin : mux_bit
-            not u_not_sel(not_sel_vec[i], sel);
-            and u_and_a(a_masked[i], a[i], not_sel_vec[i]);
-            and u_and_b(b_masked[i], b[i], sel);
-            or  u_or_y(y[i], a_masked[i], b_masked[i]);
-        end
-    endgenerate
+  );
+  wire [WIDTH-1:0] a_masked;
+  wire [WIDTH-1:0] b_masked;
+  wire [WIDTH-1:0] not_sel_vec;
+
+  // 对每一位独立实现: y[i] = (a[i] & ~sel) | (b[i] & sel)
+  genvar i;
+  generate
+    for (i = 0; i < WIDTH; i = i + 1)
+    begin : mux_bit
+      not u_not_sel(not_sel_vec[i], sel);
+      and u_and_a(a_masked[i], a[i], not_sel_vec[i]);
+      and u_and_b(b_masked[i], b[i], sel);
+      or  u_or_y(y[i], a_masked[i], b_masked[i]);
+    end
+  endgenerate
 endmodule
 
 // 4选1多路选择器 - 层次化实现
 // 使用3个2选1MUX构建
 module mux_4to1 #(
     parameter WIDTH = 32
-)(
+  )(
     input  [WIDTH-1:0] in0,
     input  [WIDTH-1:0] in1,
     input  [WIDTH-1:0] in2,
     input  [WIDTH-1:0] in3,
     input  [1:0]       sel,
     output [WIDTH-1:0] y
-);
-    wire [WIDTH-1:0] mux0_out;
-    wire [WIDTH-1:0] mux1_out;
-    
-    // 第一级: 根据sel[0]选择in0/in1和in2/in3
-    mux_2to1 #(WIDTH) mux0(
-        .a(in0),
-        .b(in1),
-        .sel(sel[0]),
-        .y(mux0_out)
-    );
-    
-    mux_2to1 #(WIDTH) mux1(
-        .a(in2),
-        .b(in3),
-        .sel(sel[0]),
-        .y(mux1_out)
-    );
-    
-    // 第二级: 根据sel[1]选择mux0_out或mux1_out
-    mux_2to1 #(WIDTH) mux_final(
-        .a(mux0_out),
-        .b(mux1_out),
-        .sel(sel[1]),
-        .y(y)
-    );
+  );
+  wire [WIDTH-1:0] mux0_out;
+  wire [WIDTH-1:0] mux1_out;
+
+  // 第一级: 根据sel[0]选择in0/in1和in2/in3
+  mux_2to1 #(WIDTH) mux0(
+             .a(in0),
+             .b(in1),
+             .sel(sel[0]),
+             .y(mux0_out)
+           );
+
+  mux_2to1 #(WIDTH) mux1(
+             .a(in2),
+             .b(in3),
+             .sel(sel[0]),
+             .y(mux1_out)
+           );
+
+  // 第二级: 根据sel[1]选择mux0_out或mux1_out
+  mux_2to1 #(WIDTH) mux_final(
+             .a(mux0_out),
+             .b(mux1_out),
+             .sel(sel[1]),
+             .y(y)
+           );
 endmodule
 
 // 8选1多路选择器 - 层次化实现
 // 使用2个4选1MUX和1个2选1MUX构建
 module mux_8to1 #(
     parameter WIDTH = 32
-)(
+  )(
     input  [WIDTH-1:0] in0,
     input  [WIDTH-1:0] in1,
     input  [WIDTH-1:0] in2,
@@ -81,43 +82,43 @@ module mux_8to1 #(
     input  [WIDTH-1:0] in7,
     input  [2:0]       sel,
     output [WIDTH-1:0] y
-);
-    wire [WIDTH-1:0] mux0_out;
-    wire [WIDTH-1:0] mux1_out;
-    
-    // 低4输入和高4输入分别选择
-    mux_4to1 #(WIDTH) mux0(
-        .in0(in0),
-        .in1(in1),
-        .in2(in2),
-        .in3(in3),
-        .sel(sel[1:0]),
-        .y(mux0_out)
-    );
-    
-    mux_4to1 #(WIDTH) mux1(
-        .in0(in4),
-        .in1(in5),
-        .in2(in6),
-        .in3(in7),
-        .sel(sel[1:0]),
-        .y(mux1_out)
-    );
-    
-    // 最终选择
-    mux_2to1 #(WIDTH) mux_final(
-        .a(mux0_out),
-        .b(mux1_out),
-        .sel(sel[2]),
-        .y(y)
-    );
+  );
+  wire [WIDTH-1:0] mux0_out;
+  wire [WIDTH-1:0] mux1_out;
+
+  // 低4输入和高4输入分别选择
+  mux_4to1 #(WIDTH) mux0(
+             .in0(in0),
+             .in1(in1),
+             .in2(in2),
+             .in3(in3),
+             .sel(sel[1:0]),
+             .y(mux0_out)
+           );
+
+  mux_4to1 #(WIDTH) mux1(
+             .in0(in4),
+             .in1(in5),
+             .in2(in6),
+             .in3(in7),
+             .sel(sel[1:0]),
+             .y(mux1_out)
+           );
+
+  // 最终选择
+  mux_2to1 #(WIDTH) mux_final(
+             .a(mux0_out),
+             .b(mux1_out),
+             .sel(sel[2]),
+             .y(y)
+           );
 endmodule
 
 // 16选1多路选择器 - 层次化实现
 // 使用2个8选1MUX和1个2选1MUX构建
 module mux_16to1 #(
     parameter WIDTH = 32
-)(
+  )(
     input  [WIDTH-1:0] in0,
     input  [WIDTH-1:0] in1,
     input  [WIDTH-1:0] in2,
@@ -136,42 +137,42 @@ module mux_16to1 #(
     input  [WIDTH-1:0] in15,
     input  [3:0]       sel,
     output [WIDTH-1:0] y
-);
-    wire [WIDTH-1:0] mux0_out;
-    wire [WIDTH-1:0] mux1_out;
-    
-    // 低8输入和高8输入分别选择
-    mux_8to1 #(WIDTH) mux0(
-        .in0(in0),
-        .in1(in1),
-        .in2(in2),
-        .in3(in3),
-        .in4(in4),
-        .in5(in5),
-        .in6(in6),
-        .in7(in7),
-        .sel(sel[2:0]),
-        .y(mux0_out)
-    );
-    
-    mux_8to1 #(WIDTH) mux1(
-        .in0(in8),
-        .in1(in9),
-        .in2(in10),
-        .in3(in11),
-        .in4(in12),
-        .in5(in13),
-        .in6(in14),
-        .in7(in15),
-        .sel(sel[2:0]),
-        .y(mux1_out)
-    );
-    
-    // 最终选择
-    mux_2to1 #(WIDTH) mux_final(
-        .a(mux0_out),
-        .b(mux1_out),
-        .sel(sel[3]),
-        .y(y)
-    );
+  );
+  wire [WIDTH-1:0] mux0_out;
+  wire [WIDTH-1:0] mux1_out;
+
+  // 低8输入和高8输入分别选择
+  mux_8to1 #(WIDTH) mux0(
+             .in0(in0),
+             .in1(in1),
+             .in2(in2),
+             .in3(in3),
+             .in4(in4),
+             .in5(in5),
+             .in6(in6),
+             .in7(in7),
+             .sel(sel[2:0]),
+             .y(mux0_out)
+           );
+
+  mux_8to1 #(WIDTH) mux1(
+             .in0(in8),
+             .in1(in9),
+             .in2(in10),
+             .in3(in11),
+             .in4(in12),
+             .in5(in13),
+             .in6(in14),
+             .in7(in15),
+             .sel(sel[2:0]),
+             .y(mux1_out)
+           );
+
+  // 最终选择
+  mux_2to1 #(WIDTH) mux_final(
+             .a(mux0_out),
+             .b(mux1_out),
+             .sel(sel[3]),
+             .y(y)
+           );
 endmodule
--- dev/1alu/rtl/subtractor.v
+++ dev/1alu/rtl/subtractor.v
@@ -5,21 +5,21 @@ module subtractor(
     input  [31:0] b,
     output [31:0] result,
     output        borrow
-);
-    wire [31:0] b_complement;
-    wire [31:0] sum;
-    wire cout;
-    
-    assign b_complement = ~b;
-    
-    cla_adder_32bit adder(
-        .a(a),
-        .b(b_complement),
-        .cin(1'b1),
-        .sum(sum),
-        .cout(cout)
-    );
-    
-    assign result = sum;
-    assign borrow = ~cout;
+  );
+  wire [31:0] b_complement;
+  wire [31:0] sum;
+  wire cout;
+
+  assign b_complement = ~b;
+
+  cla_adder_32bit adder(
+                    .a(a),
+                    .b(b_complement),
+                    .cin(1'b1),
+                    .sum(sum),
+                    .cout(cout)
+                  );
+
+  assign result = sum;
+  assign borrow = ~cout;
 endmodule

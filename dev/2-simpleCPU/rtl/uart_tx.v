95307369c8b2646aebfb3a3fe86a9d98023c1180
wood-dale-wang <cqwod@qq.com>
2026-04-25 10:20:17 +0800

    参考资料加入；uart修改

--- dev/2-simpleCPU/rtl/uart_tx.v
+++ dev/2-simpleCPU/rtl/uart_tx.v
@@ -1,25 +1,20 @@
 `timescale 1ns / 1ps
 
 module uart_tx
-#(
-    parameter CLK_FRE = 25,
-    parameter BAUD_RATE = 115200
-)
 (
     input                        clk,
     input                        reset,
+    input       [15:0]           i_clkCnt_16,
     input       [7:0]            i_txData_8,
     input                        i_txDataValid_1,
     output reg                   o_txDataReady_1,
     output                       o_txPin_1
 );
 
-    localparam CYCLE = CLK_FRE * 1000000 / BAUD_RATE;
-
-    localparam S_IDLE     = 1;
-    localparam S_START    = 2;
+    localparam S_IDLE      = 1;
+    localparam S_START     = 2;
     localparam S_SEND_BYTE = 3;
-    localparam S_STOP     = 4;
+    localparam S_STOP      = 4;
 
     reg [2:0]  state;
     reg [2:0]  next_state;
@@ -45,17 +40,17 @@ module uart_tx
                 else
                     next_state = S_IDLE;
             S_START:
-                if ($unsigned(cycle_cnt) == CYCLE - 1)
+                if (cycle_cnt == i_clkCnt_16 - 1)
                     next_state = S_SEND_BYTE;
                 else
                     next_state = S_START;
             S_SEND_BYTE:
-                if ($unsigned(cycle_cnt) == CYCLE - 1 && bit_cnt == 3'd7)
+                if (cycle_cnt == i_clkCnt_16 - 1 && bit_cnt == 3'd7)
                     next_state = S_STOP;
                 else
                     next_state = S_SEND_BYTE;
             S_STOP:
-                if ($unsigned(cycle_cnt) == CYCLE - 1)
+                if (cycle_cnt == i_clkCnt_16 - 1)
                     next_state = S_IDLE;
                 else
                     next_state = S_STOP;
@@ -72,7 +67,7 @@ module uart_tx
                 o_txDataReady_1 <= 1'b0;
             else
                 o_txDataReady_1 <= 1'b1;
-        end else if (state == S_STOP && $unsigned(cycle_cnt) == CYCLE - 1) begin
+        end else if (state == S_STOP && cycle_cnt == i_clkCnt_16 - 1) begin
             o_txDataReady_1 <= 1'b1;
         end
     end
@@ -89,7 +84,7 @@ module uart_tx
         if (reset) begin
             bit_cnt <= 3'd0;
         end else if (state == S_SEND_BYTE) begin
-            if ($unsigned(cycle_cnt) == CYCLE - 1)
+            if (cycle_cnt == i_clkCnt_16 - 1)
                 bit_cnt <= bit_cnt + 3'd1;
         end else begin
             bit_cnt <= 3'd0;
@@ -99,7 +94,7 @@ module uart_tx
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             cycle_cnt <= 16'd0;
-        end else if ((state == S_SEND_BYTE && $unsigned(cycle_cnt) == CYCLE - 1) || next_state != state) begin
+        end else if ((state == S_SEND_BYTE && cycle_cnt == i_clkCnt_16 - 1) || next_state != state) begin
             cycle_cnt <= 16'd0;
         end else begin
             cycle_cnt <= cycle_cnt + 16'd1;

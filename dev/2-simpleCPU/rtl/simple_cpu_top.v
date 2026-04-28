6c20915637327a0e64921a6d303aef2c56148136
wood-dale-wang <cqwod@qq.com>
2026-04-28 16:27:45 +0800

    mem时序问题修正，跑通斐波那契数列修正程序

--- dev/2-simpleCPU/rtl/simple_cpu_top.v
+++ dev/2-simpleCPU/rtl/simple_cpu_top.v
@@ -144,6 +144,8 @@ module simple_cpu_top(
     );
 
     cpu_fetch u_fetch(
+        .clk(clk),
+        .reset(reset),
         .if_valid(if_valid),
         .pc(pc),
         .inst_data(icache_dout),

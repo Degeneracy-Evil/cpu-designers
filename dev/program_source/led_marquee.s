2d1b7c89890ecdd4319818f6a43037775ee5e0ef
wood-dale-wang <cqwod@qq.com>
2026-05-11 13:09:33 +0800

    led程序改为基于timer中断服务程序

--- dev/program_source/led_marquee.s
+++ dev/program_source/led_marquee.s
@@ -1,36 +1,73 @@
 .equ GPIO_BASE, 0x80000000
 .equ TIMER_BASE, 0x80004000
+.equ TIMER_PERIOD, 100000000
 
 .section .text
 .globl _start
 
 _start:
-    lui x10, 0x80000
-    lui x15, 0x80004
+    # Setup mtvec
+    la t0, isr
+    csrw mtvec, t0
 
+    # Enable MTIE in mie (bit 7)
+    li t0, 0x80
+    csrw mie, t0
+
+    # Enable MIE in mstatus (bit 3)
+    li t0, 0x8
+    csrw mstatus, t0
+
+    # Initialize GPIO direction (all output)
+    lui x10, 0x80000
     li x11, 0xFFFF
     sw x11, 0(x10)
 
-    li x16, 100000000
-    sw x16, 0(x15)
-    li x16, 3
-    sw x16, 4(x15)
+    # Initialize LED state
+    li x12, 0       # x12 will be our counter (0-15)
+    li x11, 1
+    xori x13, x11, -1
+    sw x13, 4(x10)
 
-    li x12, 0
+    # Setup Timer
+    lui x15, 0x80004
+    li x16, TIMER_PERIOD
+    sw x16, 0(x15)  # expr_val = TIMER_PERIOD
+    li x16, 3
+    sw x16, 4(x15)  # start = 1, mode = 1 (periodic)
 
 loop:
+    j loop
+
+.align 4
+isr:
+    # We should preserve registers we use, but this is simple CPU, just save to mscratch
+    csrrw sp, mscratch, sp
+    addi sp, sp, -16
+    sw x11, 0(sp)
+    sw x13, 4(sp)
+    sw x14, 8(sp)
+
+    # Clear Timer IRQ
+    lui x15, 0x80004
+    sw x0, 8(x15)
+
+    # Update LED state
+    addi x12, x12, 1
+    li x11, 16
+    bne x12, x11, skip_reset
+    li x12, 0
+skip_reset:
+    lui x10, 0x80000
     li x11, 1
     sll x13, x11, x12
     xori x13, x13, -1
     sw x13, 4(x10)
 
-wait_timer:
-    lw x14, 8(x15)
-    beq x14, x0, wait_timer
-    sw x0, 8(x15)
+    lw x11, 0(sp)
+    lw x13, 4(sp)
+    lw x14, 8(sp)
+    addi sp, sp, 16
+    csrrw sp, mscratch, sp
 
-    addi x12, x12, 1
-    li x11, 16
-    bne x12, x11, loop
-    li x12, 0
-    j loop
+    mret
